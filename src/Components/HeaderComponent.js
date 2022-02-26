9b57424ab18ee380b4cd323a356793668449fa47
Vineeth Rao <vcrao@umass.edu>
2022-02-26 11:58:41 -0500

    Week 3 Fixes

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -14,6 +14,7 @@ class Header extends Component {
         this.toggleLoginModal = this.toggleLoginModal.bind(this);
         this.toggleSignUpModal = this.toggleSignUpModal.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
+        this.handleSignUp = this.handleSignUp.bind(this);
     }
 
     toggleNav() {
